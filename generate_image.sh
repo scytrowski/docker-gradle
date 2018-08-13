26b91a68143b3f898dba88500d7e13473a0b4a95
vdrake6 <vdrake6@gmail.com>
2018-08-13 23:03:40 +0200

    Remove clone dir

--- generate_image.sh
+++ generate_image.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 REPO_URL="https://github.com/vdrake6/docker-gradle.git"
 REPO_BRANCH="master"
+CLONE_DIR="repo"
 CODE_DIR="code"
 IMAGE_NAME="vdrake6/dockergradle"
 
 PROJECT_NAME=`basename ${CODE_DIR}`
 MASTER_DIR=`pwd`
 
-git clone --single-branch -b ${REPO_BRANCH} ${REPO_URL} "./repo" && \
-cd "./repo/${CODE_DIR}" && \
+rm -rf ${CLONE_DIR} && \
+git clone --single-branch -b ${REPO_BRANCH} ${REPO_URL} ${CLONE_DIR} && \
+cd "${CLONE_DIR}/${CODE_DIR}" && \
 gradle jar && \
 cp "./build/libs/${PROJECT_NAME}.jar" "${MASTER_DIR}/app.jar" && \
-docker build . -t ${IMAGE_NAME}
+cd ${MASTER_DIR} && \
+docker build . -t ${IMAGE_NAME} && \
+rm -rf ${CLONE_DIR}
